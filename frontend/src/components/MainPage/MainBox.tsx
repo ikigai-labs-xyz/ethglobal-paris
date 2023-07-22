d2a84c9e5d777bef0915ef845b32606b91448e6c
Valerio <78494380+valeriofichera@users.noreply.github.com>
2023-07-22 18:14:00 +0200

    updated frontend text
    I modified the text of the frontend to align better with the narrative of our submission at ETHGlobal ... AGAIN the initial part of this frontend has been produced before the hacakthon and is not the actual submission. Please see smart contracts and other novel parts to see what is made for the hackathon.
    
    This frontend is only needed for visual presentation purposes of the functionality that has been build for ethglobal paris 2023

--- frontend/src/components/MainPage/MainBox.tsx
+++ frontend/src/components/MainPage/MainBox.tsx
@@ -11,21 +11,21 @@ function MainBox() {
     <div className='grid grid-cols-8'>
 
       <div className='col-start-1 col-span-8 p-5'>
-        <div className="text-3xl font-extrabold text-black">TurtleShell Firewall Demo</div>
-        <div className="text-slate-500 font-bold text-lg">Learn how TurtleShell can prevent hacks</div>
+        <div className="text-3xl font-extrabold text-black">Real Time Hack Prevention</div>
+        <div className="text-slate-500 font-bold text-lg">learn how Firewalls can prevent hacks</div>
       </div>
 
       <div className='p-5 col-start-3 col-span-4 gap-5 justify-center items-center '>
 
       <a href="/no-hack" target="_blank" rel="noopener noreferrer">
         <div className="p-3 m-1 bg-black rounded-xl font-lg text-white hover:bg-slate-500">
-          Original Hack
+          without Circuit Breaker
         </div>
       </a>
 
       <a href="/hack">
         <div className="p-3 m-1 bg-black rounded-xl font-lg text-white hover:bg-slate-500">
-          Firewall-guarded Hack
+          with Circuit Breaker
         </div>
         </a>
 
@@ -35,12 +35,11 @@ function MainBox() {
 
       </div>
 
-      <div className='col-start-4 col-span-6 text-left mt-12'>
-        <div className="text-slate-500">
-          <div className='font-bold'>About</div>
-          <div className="font-base">
-              We recreated the Hundred Finance hack of March, to showcase how TurtleShell
-              could have been able to prevent $6 million from being stolen.
+      <div className='col-start-4 col-span-6 text-center mt-12'>
+        <div className="text-slate-800">
+          <div className='font-bold'>made with ❤️ @ETHGlobal Paris 2023   🇫🇷🥐</div>
+          <div className="font-sm">
+              based on TurtleShell's Firewall SDK and EIP-7265
           </div>
         </div>
       </div>
